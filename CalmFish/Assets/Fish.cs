300ed19b5ed75ac442a2ef82cfec6212060fbbe7
Nick Carroll <ncarroll@nd.edu>
2021-09-25 20:48:34 -0400

    bait and fish updates

--- CalmFish/Assets/Fish.cs
+++ CalmFish/Assets/Fish.cs
@@ -24,53 +24,12 @@ public class Fish : MonoBehaviour
 
         if (_targetedBait)
         {
-
-
-            Vector3 distance = transform.position - _targetedBait.transform.position;
-            //float distance = Vector3.Distance(transform.position, _targetedBait.transform.position);
-            //Vector3 distance = Vector3.Distance (transform.position,  _targetedBait.transform.position);
-
-
-
-            distance.z = 0;
-            if (distance.magnitude < _speed)
-            {
-                transform.position = _targetedBait.transform.position;
-            }
-            else
-            {
-                transform.position -= distance.normalized * _speed;
-            }
-
-
+            ChaseAfterBait();
         }
         else
         {
             // lazily move around
-
-
-            if (_trackingRandomTarget == false)
-            {
-                Vector3 target = transform.position + Random.insideUnitSphere;
-                target.z = 0;
-                _randomTarget = target;
-                _trackingRandomTarget = true;
-            }
-            else
-            {
-                Vector3 distance = transform.position - _randomTarget;
-                distance.z = 0;
-                if (distance.magnitude < _speed)
-                {
-                    transform.position = _randomTarget;
-                    _trackingRandomTarget = false;
-                }
-                else
-                {
-                    transform.position -= distance.normalized * _speed *0.5f;
-                }
-
-            }
+            MoveRandomly();
         }
     }
 
@@ -79,9 +38,56 @@ public class Fish : MonoBehaviour
         _targetedBait = bait;
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+
+    // move directly after the bait when it is avaiable
+    private void ChaseAfterBait()
     {
-        Vector3 direction = (transform.position - collision.gameObject.transform.position).normalized;
-        transform.position -= -direction;
+        Vector3 distance = transform.position - _targetedBait.transform.position;
+
+        distance.z = 0;
+        if (distance.magnitude < _speed)
+        {
+            transform.position = _targetedBait.transform.position;
+            _targetedBait.gameObject.SetActive(false);
+
+            // win level
+
+            // disable throwing objects
+
+            // display victory messgae
+
+            // Load next level
+        }
+        else
+        {
+            transform.position -= distance.normalized * _speed;
+        }
+    }
+
+    // randomly move when there is no bait to chase
+    private void MoveRandomly()
+    {
+        if (_trackingRandomTarget == false)
+        {
+            Vector3 target = transform.position + Random.insideUnitSphere;
+            target.z = 0;
+            _randomTarget = target;
+            _trackingRandomTarget = true;
+        }
+        else
+        {
+            Vector3 distance = transform.position - _randomTarget;
+            distance.z = 0;
+            if (distance.magnitude < _speed)
+            {
+                transform.position = _randomTarget;
+                _trackingRandomTarget = false;
+            }
+            else
+            {
+                transform.position -= distance.normalized * _speed * 0.5f;
+            }
+
+        }
     }
 }
