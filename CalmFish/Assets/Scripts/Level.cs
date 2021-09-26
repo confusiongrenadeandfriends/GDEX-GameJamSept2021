3983769c3ab8e3addc4f91c686a48b41dedba1ea
ASarrouj <asarrouj590@gmail.com>
2021-09-26 10:53:07 -0400

    added 1-1, win condition, targetFish attribute

--- CalmFish/Assets/Scripts/Level.cs
+++ CalmFish/Assets/Scripts/Level.cs
@@ -1,17 +1,16 @@
-using System.Collections.Generic;
+using System.Collections;
 using System.IO;
 using UnityEngine;
 
 public class Level : MonoBehaviour
 {
-
-    private float TILE_WIDTH = 2.0f;
-    private Vector3 originalPos = new Vector3(-5, 5, 0);
+    public GameObject winText;
+    private GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        this.initializeLevel("1-1");
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -20,37 +19,16 @@ public class Level : MonoBehaviour
         
     }
 
-    public void initializeLevel(string levelNum)
+    public void winLevel()
     {
-        var curTile = originalPos;
-        using (var reader = new StreamReader(@"Assets/Levels/" + levelNum + ".csv"))
-        {
-            while (!reader.EndOfStream)
-            {
-                var row = reader.ReadLine();
-                var elements = row.Split(',');
-
-                for (int i = 0; i < elements.Length; i++)
-                {
-                    if (dict.ContainsKey(elements[i]))
-                    {
-                        var gameEntity = dict[elements[i]];
-                        gameEntity.transform.position = curTile;
-                    }
-                    curTile.x += TILE_WIDTH;
-
-                }
-
-                curTile.x = originalPos.x;
-                curTile.y -= TILE_WIDTH;
-            }
-        }
+        winText.SetActive(true);
+        StartCoroutine(waitThenLoadNextLevel());
     }
 
-    private Dictionary<string, GameObject> dict = new Dictionary<string, GameObject>()
-{
-    {"q", GameObject.CreatePrimitive(PrimitiveType.Quad)},
-    {"r", GameObject.CreatePrimitive(PrimitiveType.Sphere)},
-};
+    private IEnumerator waitThenLoadNextLevel()
+    {
+        yield return new WaitForSeconds(4);
 
+        gameManager.LoadNextLevel();
+    }
 }
